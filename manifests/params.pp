47c9448dd6696e2b36544666b976ee38c5df058e
David Goldsmith <dgoldsmith@sans.org>
2021-07-13 16:53:43 -0400

    fix typo in software_header variable

--- manifests/params.pp
+++ manifests/params.pp
@@ -19,7 +19,7 @@ class opendkim::params {
   $statistics = '/var/spool/opendkim/stats.dat'
   $send_reports = false
   $report_address = undef
-  $softare_header = 'yes'
+  $software_header = 'yes'
   $trusted_hosts = ['::1', '127.0.0.1', 'localhost']
   $maximum_signed_bytes = undef
   $trustanchorfile  = undef
